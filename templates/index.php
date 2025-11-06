7635abd585d9e9eeb18550b72ddcea3926b6f486
CUONGDEV <cuongdev@CUONGDEVs-MacBook-Pro.local>
2025-11-06 21:22:16 +0700

    update teamplate header and footer

--- templates/index.php
+++ templates/index.php
@@ -1,1 +1,22 @@
-trang chủ nè
+<!DOCTYPE html>
+
+<html lang="<?= $config['website']['lang-doc'] ?>">
+    <head>
+
+    </head>
+    <body>
+        <div>
+            <?php 
+                include TEMPLATE . LAYOUT . "header.php";
+            ?>
+            <div class="w-clear">
+
+                <?php include TEMPLATE . $template . "_tpl.php"; ?>
+
+            </div>
+            <?php 
+                include TEMPLATE . LAYOUT . "footer.php";
+            ?>
+        </div>
+    </body>
+</html>
