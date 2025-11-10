d7097bf41bbe6fbfb048eba25dd0747abc844b3f
CUONGDEV <cuongdev@CUONGDEVs-MacBook-Pro.local>
2025-11-10 10:37:49 +0700

    update template

--- templates/layout/css.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Option 1: Load CSS trực tiếp từ file style.css
-// echo '<link href="' . ASSET . 'assets/css/style.css?v=' . time() . '" rel="stylesheet">';
-
-// Option 2: Include trực tiếp CSS từ style.php (đảm bảo CSS luôn được load)
-echo '<style>';
-include TEMPLATE . LAYOUT . 'style.php';
-echo '</style>';
-?>
