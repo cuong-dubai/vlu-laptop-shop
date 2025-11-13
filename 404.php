7282d228160cedd3501746512e11b0d2ce264a80
CUONGDEV <cuongdev@CUONGDEVs-MacBook-Pro.local>
2025-11-13 08:30:53 +0700

    update route admin,update databse table user,create structure admin dashboard

--- 404.php
+++ 404.php
@@ -1,113 +1,119 @@
 <style>
     * {
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-}
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+    }
 
-body {
-    padding: 0;
-    margin: 0;
-}
+    body {
+        padding: 0;
+        margin: 0;
+    }
 
-#notfound {
-    position: relative;
-    height: 100vh;
-}
+    #notfound {
+        position: relative;
+        height: 100vh;
+    }
 
-#notfound .notfound {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    -webkit-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    text-align: center;
-}
+    #notfound .notfound {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        text-align: center;
+    }
 
-.notfound {
-    max-width: 767px;
-    width: 100%;
-    line-height: 1.4;
-    padding: 0px 15px;
-}
+    .notfound {
+        max-width: 767px;
+        width: 100%;
+        line-height: 1.4;
+        padding: 0px 15px;
+    }
 
-.notfound .notfound-404 {
-    position: relative;
-    height: 150px;
-    line-height: 150px;
-    margin-bottom: 25px;
-    display: none;
-}
-
-.notfound .notfound-404 h1 {
-    font-family: 'Titillium Web', sans-serif;
-    font-size: 186px;
-    font-weight: 900;
-    margin: 0px;
-    text-transform: uppercase;
-    background: url('text.png');
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    background-size: cover;
-    background-position: center;
-    text-align: center;
-}
-
-.notfound h2 {
-    font-family: 'Titillium Web', sans-serif;
-    font-size: 26px;
-    font-weight: 700;
-    margin: 0;
-}
-
-.notfound p {
-    font-family: 'Montserrat', sans-serif;
-    font-size: 14px;
-    font-weight: 500;
-    margin-bottom: 0px;
-    text-transform: uppercase;
-}
-
-.notfound a {
-    display: inline-block;
-    text-transform: uppercase;
-    color: #fff;
-    text-decoration: none;
-    border: none;
-    background: #840000;
-    padding: 10px 40px;
-    font-size: 14px;
-    font-weight: 700;
-    border-radius: 1px;
-    margin-top: 15px;
-    -webkit-transition: 0.2s all;
-    transition: 0.2s all;
-    font-family: 'Montserrat', sans-serif;
-    margin: 0px auto;
-}
-
-.notfound a:hover {
-    /* opacity:0.8; */
-    border: none;
-    background: #055300;
-}
-
-@media only screen and (max-width: 767px) {
     .notfound .notfound-404 {
-        height: 110px;
-        line-height: 110px;
+        position: relative;
+        height: 150px;
+        line-height: 150px;
+        margin-bottom: 25px;
+        display: none;
     }
 
     .notfound .notfound-404 h1 {
-        font-size: 120px;
+        font-family: 'Titillium Web', sans-serif;
+        font-size: 186px;
+        font-weight: 900;
+        margin: 0px;
+        text-transform: uppercase;
+        background: url('text.png');
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-size: cover;
+        background-position: center;
+        text-align: center;
     }
-}
 
+    .notfound h2 {
+        font-family: 'Titillium Web', sans-serif;
+        font-size: 26px;
+        font-weight: 700;
+        margin: 0;
+    }
+
+    .notfound p {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 14px;
+        font-weight: 500;
+        margin-bottom: 0px;
+        text-transform: uppercase;
+    }
+
+    .notfound a {
+        display: inline-block;
+        text-transform: uppercase;
+        color: #fff;
+        text-decoration: none;
+        border: none;
+        background: #840000;
+        padding: 10px 40px;
+        font-size: 14px;
+        font-weight: 700;
+        border-radius: 1px;
+        margin-top: 15px;
+        -webkit-transition: 0.2s all;
+        transition: 0.2s all;
+        font-family: 'Montserrat', sans-serif;
+        margin: 0px auto;
+    }
+
+    .notfound a:hover {
+        /* opacity:0.8; */
+        border: none;
+        background: #055300;
+    }
+
+    @media only screen and (max-width: 767px) {
+        .notfound .notfound-404 {
+            height: 110px;
+            line-height: 110px;
+        }
+
+        .notfound .notfound-404 h1 {
+            font-size: 120px;
+        }
+    }
 </style>
 <div id="notfound">
     <div class="notfound">
         <div class="img_404">
             <img src="assets/images/404.gif" alt="404">
+        </div>
+        <div style="text-align: center; position: relative; top: -100px; font-size: 40px; color: rgb(204, 204, 204);">
+            404</div>
+        <div style="text-align: center; position: relative; top: -100px;">
+            <h3>Look like you're lost </h3>
+            <p style="margin-bottom: 50px;">The page you are looking for is currently unavailable!</p>
+            
         </div>
         <a href="<?= isset($configBase) ? $configBase : '/' ?>">Come back home !</a>
     </div>
