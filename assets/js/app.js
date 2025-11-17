6f9ceae96f65394b9453dcd7a3871474377d3ef5
CUONGDEV <cuongdev@CUONGDEVs-MacBook-Pro.local>
2025-11-17 07:48:41 +0700

    update ui register client

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,25 @@
+function validateForm(ele = "") {
+    if (ele) {
+        $("." + ele)
+            .find("input[type=submit]")
+            .removeAttr("disabled");
+        var forms = document.getElementsByClassName(ele);
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener(
+                "submit",
+                function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add("was-validated");
+                },
+                false
+            );
+        });
+    }
+}
+validateForm('validation-user');
 VLU.Slick = function(){
     $('.slideshow').slick({
       infinite: true,
