bb7ea834b4eb1f23badeb05474d55d3d0c684652
cuong-dubai <cuongdubai.dev@gmail.com>
2025-11-18 15:50:54 +0700

    update style hover card ,update ui news,update ui cart

--- templates/news/news_tpl.php
+++ templates/news/news_tpl.php
@@ -2,4 +2,26 @@
     <div class="title-main">
         <h2><?= (!empty($titleCate)) ? $titleCate : @$titleMain ?></h2>
     </div>
+    <div class="row">
+        <?php
+        foreach ($allNews as $key => $value) { ?>
+            <div class="col-12 col-sm-6 col-md-3 mb-4">
+                <a href="<?=$value['slug']?>">
+                    <div class="card news-card border-0">
+                        <div class="card-img-top overflow-hidden">
+                            <img src="<?= UPLOAD_PRODUCT_L . $value['photo'] ?>" width="400" height="250"
+                                onerror="this.src='assets/images/noimage.png'" class="" alt="<?= $value['name'] ?>">
+                        </div>
+                        <div class="card-body px-0 pt-2">
+                            <h6 class="card-title mb-0">
+                                <?= $value['name'] ?>
+                            </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        <?php }
+        ?>
+
+    </div>
 </div>
